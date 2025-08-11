bba279e54a10295239792aa7300d4086634f0c1f
Reiner Huechting <dev@reinerhuechting.de>
2025-08-11 17:00:51 +0200

    feat: Add more constructors for `SourceCode` from lists of strings.

---
+++ src/source_code/constructors/from_string_lists.rs
@@ -0,0 +1,82 @@
+use super::SourceCode;
+
+impl From<Vec<String>> for SourceCode {
+    fn from(value: Vec<String>) -> Self {
+        Self { lines: value }
+    }
+}
+
+impl From<&Vec<String>> for SourceCode {
+    fn from(value: &Vec<String>) -> Self {
+        Self {
+            lines: value.clone(),
+        }
+    }
+}
+
+impl From<&[String]> for SourceCode {
+    fn from(value: &[String]) -> Self {
+        Self {
+            lines: value.iter().map(|s| s.to_string()).collect(),
+        }
+    }
+}
+
+impl From<&[&str]> for SourceCode {
+    fn from(value: &[&str]) -> Self {
+        Self {
+            lines: value.iter().map(|s| s.to_string()).collect(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXPECTED_CONTENT: &str = "fn main() {\n  println!(\"Hello World\")\n}";
+
+    #[test]
+    fn from_vec() {
+        let input_lines = vec![
+            "fn main() {".to_string(),
+            "  println!(\"Hello World\")".to_string(),
+            "}".to_string(),
+        ];
+
+        let code = SourceCode::from(input_lines);
+        assert_eq!(code.content(), EXPECTED_CONTENT);
+    }
+
+    #[test]
+    fn from_vec_ref() {
+        let input_lines = vec![
+            "fn main() {".to_string(),
+            "  println!(\"Hello World\")".to_string(),
+            "}".to_string(),
+        ];
+
+        let code = SourceCode::from(&input_lines);
+        assert_eq!(code.content(), EXPECTED_CONTENT);
+    }
+
+    #[test]
+    fn from_str_slice() {
+        let input_lines = ["fn main() {", "  println!(\"Hello World\")", "}"];
+
+        let code = SourceCode::from(input_lines.as_slice());
+        assert_eq!(code.content(), EXPECTED_CONTENT);
+    }
+
+    #[test]
+    fn from_string_slice() {
+        let input_lines = [
+            "fn main() {".to_string(),
+            "  println!(\"Hello World\")".to_string(),
+            "}".to_string(),
+        ];
+
+        let code = SourceCode::from(input_lines.iter().as_slice());
+        assert_eq!(code.content(), EXPECTED_CONTENT);
+    }
+}
---
+++ src/source_code/constructors/mod.rs
@@ -0,0 +1,4 @@
+use super::SourceCode;
+
+mod from_string_lists;
+mod from_strings;
