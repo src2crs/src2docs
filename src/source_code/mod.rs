6db5e7917e6299866cc0ac1af4c77d3b058d505b
Reiner Huechting <dev@reinerhuechting.de>
2025-08-11 16:35:43 +0200

    refactor: Move `SourceCode` constructors into their own module.

--- src/source_code/mod.rs
+++ src/source_code/mod.rs
@@ -9,47 +9,5 @@ impl SourceCode {
     }
 }
 
-impl From<String> for SourceCode {
-    fn from(value: String) -> Self {
-        Self::from(value.as_str())
-    }
-}
-
-impl From<&String> for SourceCode {
-    fn from(value: &String) -> Self {
-        Self::from(value.as_str())
-    }
-}
-
-impl From<&str> for SourceCode {
-    fn from(value: &str) -> Self {
-        Self {
-            lines: value.split("\n").map(|s| s.to_string()).collect(),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn from_str() {
-        let code = SourceCode::from("fn main() {}");
-        assert_eq!(code.content(), "fn main() {}");
-    }
-
-    #[test]
-    fn from_string() {
-        let code = SourceCode::from("fn main() {}".to_string());
-        assert_eq!(code.content(), "fn main() {}");
-    }
-
-    #[test]
-    fn from_string_ref() {
-        let code = SourceCode::from(&"fn main() {}".to_string());
-        assert_eq!(code.content(), "fn main() {}");
-    }
-}
-
+mod constructors;
 mod examples;
